4d91c92b98ee35d6305b51eae1fba6b5775edc1a
Oleguer Carreras <oleguer.6.bdn@hotmail.com>
2021-10-15 19:24:18 +0200

    Added node modules

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  rules: {
+    "no-param-reassign": "off",
+    "no-plusplus": "off",
+    "no-restricted-syntax": "off",
+    "consistent-return": "off",
+    "no-debugger": "off",
+    "no-console": "off",
+  },
+  parser: "@babel/eslint-parser",
+};
