c416cb283d5aa1c3b0c3613b0dc23c9096beb7e9
Oleguer Carreras <oleguer.6.bdn@hotmail.com>
2021-10-17 00:24:39 +0200

    Added Constructor, createCardsHTML and fillCardList methods to Pokeball class

--- public/src/Pokeball.js
+++ public/src/Pokeball.js
@@ -1,1 +1,48 @@
-class Pokeball {}
+import PokemonService from "./PokemonService.js";
+import Card from "./Card.js";
+
+class Pokeball {
+  cardsParentElement;
+
+  paginationParentElement;
+
+  cardsListElement;
+
+  paginationElement;
+
+  pokemonService;
+
+  pokemonList;
+
+  url = "https://oleguer-pokemon.herokuapp.com/pokemon";
+
+  totalPokemons;
+
+  constructor(cardsParentElement) {
+    this.pokemonService = new PokemonService();
+    this.cardsParentElement = cardsParentElement;
+    this.createCardsHTML();
+    this.fillCardList();
+  }
+
+  createCardsHTML() {
+    this.cardsListElement = document.createElement("ul");
+    this.cardsListElement.className = "cards__list";
+    this.cardsParentElement.appendChild(this.cardsListElement);
+  }
+
+  fillCardList() {
+    this.cardsListElement.innerHTML = "";
+    (async () => {
+      this.pokemonList = await this.pokemonService.getPokemons(this.url);
+      console.log(this.pokemonList);
+      this.totalPokemons = this.pokemonList.length;
+      console.log(this.totalPokemons);
+      this.pokemonList.map(
+        (pokemon) => new Card(this.cardsListElement, pokemon.name, pokemon.url)
+      );
+    })();
+  }
+}
+
+export default Pokeball;
