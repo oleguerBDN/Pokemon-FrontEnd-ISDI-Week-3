a9083e0c11f53662493282f067ab6b5413aa0b0c
Oleguer Carreras <oleguer.6.bdn@hotmail.com>
2021-10-16 13:52:55 +0200

    Added Navbar class with constructor and createHTML method

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,8 +1,31 @@
-import Page from "./Page.js";
+class Navbar {
+  parentElement;
 
-class Navbar extends Page {
-  constructor() {
-    super();
+  navbarList;
+
+  constructor(parentElement, navbarList) {
+    this.parentElement = parentElement;
+    this.navbarList = navbarList;
+    this.createNavbarHTML();
+  }
+
+  createNavbarHTML() {
+    const navElement = document.createElement("nav");
+    navElement.className = "navbar";
+    const ulElement = document.createElement("ul");
+    ulElement.className = "navbar";
+    this.parentElement.appendChild(navElement);
+    navElement.appendChild(ulElement);
+
+    this.navbarList.forEach((listItem) => {
+      const newItem = document.createElement("li");
+      newItem.className = "navbar__item";
+      ulElement.appendChild(newItem);
+      const newArrow = document.createElement("a");
+      newArrow.href = listItem.href;
+      newArrow.textContent = listItem.text;
+      newItem.appendChild(newArrow);
+    });
   }
 }
 
