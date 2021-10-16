874a22e294f9a2a4fab25e27d034325206bc7f0c
Oleguer Carreras <oleguer.6.bdn@hotmail.com>
2021-10-16 18:07:34 +0200

    Added create pagination html method and eventlisteners to left and right button

--- public/src/Cards.js
+++ public/src/Cards.js
@@ -2,9 +2,13 @@ import PokemonService from "./PokemonService.js";
 import Card from "./Card.js";
 
 class Cards {
-  parentElement;
+  cardsParentElement;
+
+  paginationParentElement;
 
   cardsListElement;
+
+  paginationElement;
 
   pageType;
 
@@ -12,25 +16,48 @@ class Cards {
 
   pokemonList;
 
-  constructor(parentElement, pageType) {
+  url = "https://pokeapi.co/api/v2/pokemon?limit=12&offset=0";
+
+  constructor(cardsParentElement, paginationParentElement, pageType) {
     this.pokemonService = new PokemonService();
-    this.parentElement = parentElement;
+    this.cardsParentElement = cardsParentElement;
+    this.paginationParentElement = paginationParentElement;
     this.pageType = pageType;
     this.createCardsHTML();
     this.fillCardList();
+    this.createPaginationHTML();
+    this.addPaginationListeners();
   }
 
   createCardsHTML() {
     this.cardsListElement = document.createElement("ul");
     this.cardsListElement.className = "cards__list";
-    this.parentElement.appendChild(this.cardsListElement);
+    this.cardsParentElement.appendChild(this.cardsListElement);
+  }
+
+  createPaginationHTML() {
+    this.paginationElement = document.createElement("ul");
+    this.paginationElement.className = "pagination";
+    this.paginationParentElement.appendChild(this.paginationElement);
+    this.paginationElement.innerHTML = `<li class="pagination__item pagination__item--left"><<</li>
+            <li class="pagination__item pagination__item--right">>></li>`;
+  }
+
+  addPaginationListeners() {
+    const leftButton = this.paginationElement.querySelector(
+      ".pagination__item--left"
+    );
+    const rightButton = this.paginationElement.querySelector(
+      ".pagination__item--right"
+    );
+
+    leftButton.addEventListener("click", this.leftClicked);
+    rightButton.addEventListener("click", this.rightClicked);
   }
 
   fillCardList() {
     (async () => {
-      this.pokemonList = await this.pokemonService.getPokemons(
-        "https://pokeapi.co/api/v2/pokemon?limit=12&offset=0"
-      );
+      this.pokemonList = await this.pokemonService.getPokemons(this.url);
       this.pokemonList.results.map(
         (pokemon) => new Card(this.cardsListElement, pokemon.name, pokemon.url)
       );
