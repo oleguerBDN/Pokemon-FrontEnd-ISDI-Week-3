62bdfc087fe9939628da3c959eebd0b5c005a452
Oleguer Carreras <oleguer.6.bdn@hotmail.com>
2021-10-16 19:29:25 +0200

    Added pagination basic testing

--- public/src/Cards.test.js
+++ public/src/Cards.test.js
@@ -1,7 +1,7 @@
 import Cards from "./Cards";
 
 describe("Given a new Cards class ", () => {
-  describe("When it receives a parent div and any page type", () => {
+  describe("When it receives a two parent div and any page type", () => {
     test("Then it should render a list with classname 'cards__list'", () => {
       const container = document.createElement("div");
       const container2 = document.createElement("div");
@@ -9,6 +9,17 @@ describe("Given a new Cards class ", () => {
       new Cards(container, container2, "index");
 
       const searchUl = container.querySelector("ul.cards__list");
+
+      expect(searchUl).not.toBeNull();
+    });
+
+    test("Then it should render a list with classname 'pagination' under second container", () => {
+      const container = document.createElement("div");
+      const container2 = document.createElement("div");
+
+      new Cards(container, container2, "index");
+
+      const searchUl = container2.querySelector("ul.pagination");
 
       expect(searchUl).not.toBeNull();
     });
